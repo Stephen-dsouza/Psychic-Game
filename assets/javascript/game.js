6318a49295e338d290df11d12418692cc05c51a5
Stephen D <stephen.roy.dsouza@gmail.com>
2019-07-05 19:33:13 +1000

    added logic to increase chance if correct guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ var comp = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
 var score = 0;
 var chances = 4;
 var keypresscapture = [];
+var computerguessarray=[];
 
 
 // Get the length of the array
@@ -26,20 +27,24 @@ var computersguess = guess();
 document.onkeydown = function (event) {
   var userinput = event.key.toLowerCase();
   keypresscapture.push(event.key.toLowerCase());
-
+  
+  console.log(computerguessarray);
   console.log(computersguess);
-
   //Output the user key press
   document.getElementById("userinput1").innerHTML = "You entered: " + keypresscapture;
 
-  //If userinput is same as computerguess then up the score and display alert
+  //If userinput is same as computerguess then up the score,increase the chance and display alert
 
   if (userinput === computersguess && chances > 0) {
+    computerguessarray.push(computersguess);
     score++;
     document.getElementById("score").innerHTML = "Score is: " + score
-    alert("Congratulations");
+    alert("Congratulations. Your chances have been reset");
+    document.getElementById("computerguessed").innerHTML = "Computer Guess: " + computerguessarray;
     computersguess = guess();
-
+    chances++;
+    document.getElementById("chancesleft").innerHTML = "Chances left: " + chances;
+   
   }
 
   //else if chances still less than 4 
@@ -51,8 +56,8 @@ document.onkeydown = function (event) {
 
   //else if no chances left
   else {
-    // document.getElementById("computerguessed").innerHTML = "Computer guessed character: " + computercapture;
     document.getElementById("gameover").innerHTML = "GAME OVER.<br>Press F5 to start a new game. ";
+    document.getElementById("computerguessed").innerHTML = "Chances left: " + computerguessarray;
     stop();
   }
 }
